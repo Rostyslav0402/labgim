0cca4a683c4aaec2195c20448506fe31502a398a
Rostyslav0402 <rostyslav.myshok.ki.2021@lpnu.ua>
2024-12-10 22:33:49 +0200

    labs

---
+++ mylabs/urls.py
@@ -0,0 +1,37 @@
+"""
+URL configuration for mylabs project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.shortcuts import render
+from django.urls import path
+
+from mylabs import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+def index(request):
+    return render(request, 'index.html')  # Шлях до вашого шаблону
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),  # Головна сторінка
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
